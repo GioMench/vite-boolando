acfdb1dc4f8bd1a6e360ed4afcc74f118731e041
GioMench <giorgiaglm@gmail.com>
2024-03-21 16:25:27 +0100

    add img in public and ad components AppHeader to App without style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,35 @@
-<script setup>
+<script>
+export default {
+    name: 'AppHeader'
+}
 
 </script>
 
 <template>
-  
+  <nav class="container">
+            <div class="srcbar">
+                <div class="type">
+                    <ul>
+                        <li><a class="nav-item" href="#">Uomo</a></li>
+                        <li><a class="nav-item" href="#">Donna</a></li>
+                        <li><a class="nav-item" href="#">Bambini</a></li>
+                    </ul>
+                </div>
+                <!--type-->
+                <div class="logo">
+                    <img src="/img/boolean-logo.png">
+                </div>
+                <!--logo-->
+                <div class="private">
+                    <ul>
+                        <li><i class="fa-regular fa-user"></i></li>
+                        <li><i class="fa-regular fa-heart"></i></li>
+                        <li><i class="fa-solid fa-bag-shopping"></i></li>
+                    </ul>
+                </div>
+                <!--private-->
+            </div>
+        </nav>
 </template>
 
 <style>
