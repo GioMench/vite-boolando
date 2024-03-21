f0c4f89cc0b978e4da26193f1425ffd66c5c5e8e
GioMench <giorgiaglm@gmail.com>
2024-03-21 17:12:49 +0100

    add appMain and Footer without style and package sass

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,9 +1,36 @@
-<script setup>
-
+<script>
+export default {
+    name: 'AppFooter'
+}
 </script>
 
 <template>
-  
+   <nav class="container">
+            <div class="more">
+                <div class="srl">
+                    <div>
+                        <strong> Booleando s.r.l</strong>
+                    </div>
+                    <ul class="ul2">
+                        <li>Informazioni legali</li>
+                        <li>Informativa sulla privacy</li>
+                        <li>Direitto di recesso</li>
+                    </ul>
+                </div>
+                <div class="social">
+                    <div class="find-us"><strong>Trovaci anche su</strong></div>
+                    <ul class="ul2">
+                        <li><i class="fab fa-twitter"></i></li>
+                        <li><i class="fab fa-facebook"></i></li>
+                        <li><i class="fab fa-instagram"></i></li>
+                        <li><i class="fab fa-pinterest"></i></li>
+                        <li><i class="fab fa-youtube"></i></li>
+                    </ul>
+                </div>
+            </div>
+
+
+        </nav>
 </template>
 
 <style>
