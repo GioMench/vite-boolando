51d193650f0952deb6b8dfb8535ce02eab53dfc3
GioMench <giorgiaglm@gmail.com>
2024-03-22 15:32:53 +0100

    create component AppProduct

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,10 @@
 <script>
+import AppProduct from '../components/AppProduct.vue'
 export default {
-    name: 'AppMain'
+    name: 'AppMain',
+    components:{
+        AppProduct,
+    }
 }
 </script>
 
@@ -10,35 +14,8 @@ export default {
         <div class="container m-5 p-0">
             <div class="row row-cols-1 row-cols-sm-3 row-cols-md-4">
 
-                <div class="card m-3 p-0" style="width: 18rem;">
+                <AppProduct />
 
-                    <img src="/img/1.webp" class="card-img-top front-image" alt="...">
-
-                    <img src="/img/1b.webp" class="card-img-top back-image"  alt="">
-                    
-                    <div class="card-body top">
-                        <div class="like">
-                            <i class="fa-solid fa-heart"></i>
-                        </div>
-
-                        <div class="sales">
-                            -50%
-                        </div>
-
-                        <div class="sustainable">
-                            Sostenibilità
-                        </div>
-
-                        <div>Levi's</div>
-                        <h5 class="card-title">RELAXED FIT TEE UNISEX</h5>
-                        <div class="card-text"> 14,99
-                            <span class="price">
-                                <del class="text-danger">29,99&euro;</del>
-                            </span>
-                        </div>
-
-                    </div>
-                </div>
             </div>
         </div>
     </main>
---
+++ src/components/AppProduct.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    name: 'AppProduct'
+}
+</script>
+
+<template>
+    <div class="card m-3 p-0" style="width: 18rem;">
+
+        <img src="/img/1.webp" class="card-img-top front-image" alt="...">
+
+        <img src="/img/1b.webp" class="card-img-top back-image" alt="">
+
+        <div class="card-body top">
+            <div class="like">
+                <i class="fa-solid fa-heart"></i>
+            </div>
+
+            <div class="sales">
+                -50%
+            </div>
+
+            <div class="sustainable">
+                Sostenibilità
+            </div>
+
+            <div>Levi's</div>
+            <h5 class="card-title">RELAXED FIT TEE UNISEX</h5>
+            <div class="card-text"> 14,99
+                <span class="price">
+                    <del class="text-danger">29,99&euro;</del>
+                </span>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss"></style>
