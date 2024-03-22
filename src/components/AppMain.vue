2570c52b7467364b7b0ee118249f18ba3eee05f1
GioMench <giorgiaglm@gmail.com>
2024-03-22 15:20:48 +0100

    set card type

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,240 +5,42 @@ export default {
 </script>
 
 <template>
+
     <main>
-        <div class="products">
-            <div class="container">
-                <div class="row">
+        <div class="container m-5 p-0">
+            <div class="row row-cols-1 row-cols-sm-3 row-cols-md-4">
 
-                    <!--first-->
-                    <div class="col-4">
-                        <div class="card">
-                            <div class="top">
-
-                                <img class="front-image" src="/img/1.webp" alt="">
-                                <img class="back-image" src="/img/1b.webp" alt="">
-
-                                <div class="like">
-                                    <i class="fa-solid fa-heart"></i>
-                                </div>
-
-                                <div class="sales">
-                                    -50%
-                                </div>
-
-                                <div class="sustainable">
-                                    Sostenibilità
-                                </div>
-                            </div>
-                            <div class="describe">
-
-                                <div class="brand">
-                                    Levi's
-                                </div>
-                                <div class="clothing-name">
-                                    <strong>RELAXED FIT TEE UNISEX</strong>
-                                </div>
-                                <div class="sales-price">
-                                    14,99
-                                    <span class="price">
-                                        <del>29,99&euro;</del>
-                                    </span>
-                                </div>
-                            </div>
-
-
-
+                <div class="card m-3 p-0" style="width: 18rem;">
+                    <img src="/img/1.webp" class="card-img-top" alt="...">
+                    <div class="card-body top">
+                        <div class="like">
+                            <i class="fa-solid fa-heart"></i>
                         </div>
-                    </div>
 
-                    <!--second-->
-                    <div class="col-4">
-                        <div class="card">
-                            <div class="top">
-
-                                <img class="front-image" src="/img/2.webp" alt="">
-                                <img class="back-image" src="/img/2b.webp" alt="">
-
-                                <div class="like">
-                                    <i class="fa-solid fa-heart"></i>
-                                </div>
-
-                                <div class="sales">
-                                    -30%
-                                </div>
-
-                            </div>
-                            <div class="describe">
-
-                                <div class="brand">
-                                    Guess
-                                </div>
-                                <div class="clothing-name">
-                                    <strong>ROSES TEE</strong>
-                                </div>
-                                <div class="sales-price">
-                                    20,99
-                                    <span class="price">
-                                        <del>29,99&euro;</del>
-                                    </span>
-                                </div>
-                            </div>
-
-
-
+                        <div class="sales">
+                            -50%
                         </div>
-                    </div>
 
-                    <!--third-->
-                    <div class="col-4">
-                        <div class="card">
-                            <div class="top">
-
-                                <img class="front-image" src="/img/3.webp" alt="">
-                                <img class="back-image" src="/img/3b.webp" alt="">
-
-                                <div class="like">
-                                    <i class="fa-solid fa-heart"></i>
-                                </div>
-
-                                <div class="sales">
-                                    -30%
-                                </div>
-
-                            </div>
-                            <div class="describe">
-
-                                <div class="brand">
-                                    Come Zucchero Filato
-                                </div>
-                                <div class="clothing-name">
-                                    <strong>VOGLIA DI COLORI PASTELLO</strong>
-                                </div>
-                                <div class="sales-price">
-                                    129,99
-                                    <span class="price">
-                                        <del>184,99&euro;</del>
-                                    </span>
-                                </div>
-                            </div>
-
-
-
+                        <div class="sustainable">
+                            Sostenibilità
                         </div>
-                    </div>
 
-                    <!--fouth-->
-                    <div class="col-4">
-                        <div class="card">
-                            <div class="top">
-
-                                <img class="front-image" src="/img/4.webp" alt="">
-                                <img class="back-image" src="/img/4b.webp" alt="">
-
-                                <div class="like">
-                                    <i class="fa-solid fa-heart"></i>
-                                </div>
-
-                                <div class="sales">
-                                    -50%
-                                </div>
-
-                                <div class="sustainable">
-                                    Sostenibilità
-                                </div>
-                            </div>
-                            <div class="describe">
-
-                                <div class="brand">
-                                    Levi's
-                                </div>
-                                <div class="clothing-name">
-                                    <strong>TEE UNISEX</strong>
-                                </div>
-                                <div class="sales-price">
-                                    14,99
-                                    <span class="price">
-                                        <del>29,99&euro;</del>
-                                    </span>
-                                </div>
-                            </div>
-
-
-
+                        <div>Levi's</div>
+                        <h5 class="card-title">RELAXED FIT TEE UNISEX</h5>
+                        <div class="card-text"> 14,99
+                            <span class="price">
+                                <del class="text-danger">29,99&euro;</del>
+                            </span>
                         </div>
+
                     </div>
-
-                    <!--fifth-->
-                    <div class="col-4">
-                        <div class="card">
-                            <div class="top">
-
-                                <img class="front-image" src="/img/5.webp" alt="">
-                                <img class="back-image" src="/img/5b.webp" alt="">
-
-                                <div class="like">
-                                    <i class="fa-solid fa-heart"></i>
-                                </div>
-
-                            </div>
-                            <div class="describe">
-
-                                <div class="brand">
-                                    Maya Delux
-                                </div>
-                                <div class="clothing-name">
-                                    <strong>STRIPE BODICE</strong>
-                                </div>
-                                <div class="sales-price">
-                                    99,99&euro;
-                                </div>
-                            </div>
-
-
-
-                        </div>
-                    </div>
-
-                    <!--sixth-->
-                    <div class="col-4">
-                        <div class="card">
-                            <div class="top">
-
-                                <img class="front-image" src="/img/6.webp" alt="">
-                                <img class="back-image" src="/img/6b.webp" alt="">
-
-                                <div class="like">
-                                    <i class="fa-solid fa-heart"></i>
-                                </div>
-
-                                <div class="sustainable last">
-                                    Sostenibilità
-                                </div>
-                            </div>
-                            <div class="describe">
-
-                                <div class="brand">
-                                    Esprit
-                                </div>
-                                <div class="clothing-name">
-                                    <strong>MAGLIONE-BLACK</strong>
-                                </div>
-                                <div class="sales-price">
-                                    29,99&euro;
-                                </div>
-                            </div>
-
-
-
-                        </div>
-                    </div>
-
                 </div>
-
             </div>
-
         </div>
     </main>
+
+
+
 </template>
 
-<style></style>
+<style lang="scss"></style>
