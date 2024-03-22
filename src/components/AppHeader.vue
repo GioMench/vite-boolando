a9f9df3a95369a717d5a303ab2814602230de95e
GioMench <giorgiaglm@gmail.com>
2024-03-22 12:41:08 +0100

    fix color header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,18 +7,18 @@ export default {
 
 <template>
     <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary" style="min-height: 50px;">
+        <nav class="navbar navbar-expand-lg" style="min-height: 50px;">
             <div class="container-fluid position-relative">
                 <div class="collapse navbar-collapse " id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Uomo</a>
+                        <li class="nav-item ">
+                            <a class="nav-link active text-light" aria-current="page" href="#">Uomo</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Donna</a>
+                            <a class="nav-link active text-light" aria-current="page" href="#">Donna</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Bambino</a>
+                            <a class="nav-link active text-light" aria-current="page" href="#">Bambino</a>
                         </li>
 
                     </ul>
@@ -29,17 +29,17 @@ export default {
                 <div class="collapse navbar-collapse position-absolute bottom-0 end-0" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">
+                            <a class="nav-link active text-light" aria-current="page" href="#">
                                 <i class="fa-regular fa-user"></i>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">
+                            <a class="nav-link active text-light" aria-current="page" href="#">
                                 <i class="fa-regular fa-heart"></i>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">
+                            <a class="nav-link active text-light" aria-current="page" href="#">
                                 <i class="fa-solid fa-bag-shopping"></i>
                             </a>
                         </li>
