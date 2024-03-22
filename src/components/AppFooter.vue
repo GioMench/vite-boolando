b8e4863abb05c5f52c496e686c1b3b20cf20eeef
GioMench <giorgiaglm@gmail.com>
2024-03-22 16:26:15 +0100

    set footer style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,32 +6,30 @@ export default {
 
 <template>
     <footer>
-        <nav class="container">
-            <div class="more">
-                <div class="srl">
-                    <div>
-                        <strong> Booleando s.r.l</strong>
-                    </div>
-                    <ul class="ul2">
-                        <li>Informazioni legali</li>
-                        <li>Informativa sulla privacy</li>
-                        <li>Direitto di recesso</li>
-                    </ul>
+        <div class="container d-flex justify-content-between">
+
+            <div class="srl">
+                <div class="text-light">
+                    <strong> Booleando s.r.l</strong>
                 </div>
-                <div class="social">
-                    <div class="find-us"><strong>Trovaci anche su</strong></div>
-                    <ul class="ul2">
-                        <li><i class="fab fa-twitter"></i></li>
-                        <li><i class="fab fa-facebook"></i></li>
-                        <li><i class="fab fa-instagram"></i></li>
-                        <li><i class="fab fa-pinterest"></i></li>
-                        <li><i class="fab fa-youtube"></i></li>
-                    </ul>
-                </div>
+                <ul class="ul2 text-light">
+                    <li>Informazioni legali</li>
+                    <li>Informativa sulla privacy</li>
+                    <li>Direitto di recesso</li>
+                </ul>
             </div>
-
-
-        </nav>
+            <div class="social">
+                <div class="find-us text-light "><strong>Trovaci anche su</strong></div>
+                <ul class="ul2 d-flex text-light">
+                    <li><i class="fab fa-twitter m-2"></i></li>
+                    <li><i class="fab fa-facebook m-2"></i></li>
+                    <li><i class="fab fa-instagram m-2"></i></li>
+                    <li><i class="fab fa-pinterest m-2"></i></li>
+                    <li><i class="fab fa-youtube m-2"></i></li>
+                </ul>
+            </div>
+        </div>
+       
     </footer>
 </template>
 
